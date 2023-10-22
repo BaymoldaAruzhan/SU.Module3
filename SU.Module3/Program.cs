35fa3063e16b9c252a7e503e02b85f91656d15d4
Aru2003 <baymolda03@inbox.ru>
2023-10-22 16:32:53 +0600

    Добавьте файлы проекта.

---
+++ SU.Module3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //1. Напечатать весь массив целых чисел:
+        int[] numbers = { 10, 20, 15, 25, 5 };
+        foreach (int number in numbers)
+        {
+            Console.WriteLine("1 Задача:",number);
+        }
+        //2.Найти индекс максимального значения в массиве:
+        int maxIndex = Array.IndexOf(numbers, numbers.Max());
+        Console.WriteLine("Индекс максимального значения: " + maxIndex);
+    }
+}
