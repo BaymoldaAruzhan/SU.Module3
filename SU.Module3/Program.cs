1bdaa4a2b46999080e812486fe233c36155f6091
Aru2003 <baymolda03@inbox.ru>
2023-10-22 16:40:39 +0600

    Индекс максимального четного значения

--- SU.Module3/Program.cs
+++ SU.Module3/Program.cs
@@ -15,6 +15,11 @@ class Program
         Console.WriteLine("2 Задание:");
         int maxIndex = Array.IndexOf(numbers, numbers.Max());
         Console.WriteLine("Индекс максимального значения: " + maxIndex);
+        //3.Найти индекс максимального четного значения в массиве:
+        Console.WriteLine("3 Задание:");
+        int maxEvenIndex = Array.IndexOf(numbers, numbers.Where(n => n % 2 == 0).Max());
+        Console.WriteLine("Индекс максимального четного значения: " + maxEvenIndex);
+
     }
 
 }
